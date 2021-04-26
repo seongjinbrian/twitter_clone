03130f218991ddf60169cd1d73338fa2ccb69cc1
seongjinbrian <seongjin2788@gmail.com>
2021-04-26 11:44:20 +0900

    Add user class

--- back/app.py
+++ back/app.py
@@ -1,7 +1,26 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+
+Base = SQLAlchemy(app)
+
+class Users(Base.Model):
+    id = Base.Column('student_id', Base.Integer, primary_key = True)
+    name = Base.Column(Base.String(24))
+    email = Base.Column(Base.String(64))
+    passwordd = Base.Column(Base.String(64))
+
+    # Constructor
+    def __init__(self, username, email, pwd):
+        self.username = username
+        self.email = email
+        self.password = pwd
 
 @app.route('/')
 def index():
-    return "Hello, world"
+    return "Hello, world!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ back/router/User.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+class Users(db.Model):
+    id = db.Column('student_id', db.Integer, primary_key = True) # primary_key makes it so that this value is unique and can be used to identify this record.
+    username = db.Column(db.String(24))
+    email = db.Column(db.String(64))
+    pwd = db.Column(db.String(64))
+
+    # Constructor
+    def __init__(self, username, email, pwd):
+        self.username = username
+        self.email = email
+        self.pwd = pwd
